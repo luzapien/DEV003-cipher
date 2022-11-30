3ea126dd53f283e6d9a5a71acfc1861154c718c3
María Rodríguez <lu.rzapien.92@gmail.com>
2022-11-30 15:46:09 -0600

    added encode/decode functions

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,38 @@
 const cipher = {
-  // ...
+  encode: function (offsetValue, textareaValue) {
+    let output = "";
+    
+    for (let i = 0; i < textareaValue.length; i++) {
+      let char = textareaValue[i];
+      const code = textareaValue.charCodeAt(i);
+
+      if (code >= 65 && code <= 90) {
+        char = String.fromCharCode(((code - 65 + offsetValue) % 26) + 65);
+      }
+      
+      output = output + char;
+    }
+
+    return output;
+  },
+  decode: function (offsetValue, textareaValue) {
+    let output = "";
+
+    for (let i = 0; i < textareaValue.length; i++) {
+      let char = textareaValue[i];
+      const code = textareaValue.charCodeAt(i);
+
+      if (code >= 65 && code <= 90) {
+        char = String.fromCharCode(((code - 90 - offsetValue) % 26) + 90);
+      }
+      
+      output = output + char;
+    }
+
+    return output;
+  }
 };
+
+
 
 export default cipher;
