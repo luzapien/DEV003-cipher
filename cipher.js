b2dd0e305c6d86e11f8d8baa8de816c3e685adcb
María Rodríguez <lu.rzapien.92@gmail.com>
2022-12-05 13:35:35 -0600

    Updates

--- cipher.js
+++ cipher.js
@@ -3,8 +3,8 @@ const cipher = {
     if (typeof offsetValue !== "number" || typeof textareaValue !== "string") {
       throw new TypeError("Tipos de datos no válidos", "cipher.js", 3);
     }
-    let output = "";
 
+    let output = "";
     for (let i = 0; i < textareaValue.length; i++) {
       let char = textareaValue[i];
       const code = textareaValue.charCodeAt(i);
@@ -13,10 +13,9 @@ const cipher = {
         char = String.fromCharCode(((code - 65 + offsetValue) % 26) + 65);
       } else if (code >= 97 && code <= 122) {
         char = String.fromCharCode(((code - 97 + offsetValue) % 26) + 97);
-
       }
-      output = output + char;
 
+      output = output + char;
     }
 
     return output;
@@ -25,8 +24,8 @@ const cipher = {
     if (typeof offsetValue !== "number" || typeof textareaValue !== "string") {
       throw new TypeError("Tipos de datos no válidos", "cipher.js", 25);
     }
-    let output = "";
 
+    let output = "";
     for (let i = 0; i < textareaValue.length; i++) {
       let char = textareaValue[i];
       const code = textareaValue.charCodeAt(i);
@@ -35,7 +34,6 @@ const cipher = {
         char = String.fromCharCode(((code - 90 - offsetValue) % 26) + 90);
       } else if (code >= 97 && code <= 122) {
         char = String.fromCharCode(((code - 122 - offsetValue) % 26) + 122);
-
       }
 
       output = output + char;
@@ -44,7 +42,5 @@ const cipher = {
     return output;
   }
 };
-
-
 
 export default cipher;
