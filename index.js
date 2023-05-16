8cbf79087c711932de25a767992910f9e3da68b5
María Rodríguez <lu.rzapien.92@gmail.com>
2023-05-16 16:35:17 -0600

    Updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const decipherBtn = document.getElementById("decipher");
 cipherBtn.addEventListener("click", function () {
   const offsetValue = parseInt(document.getElementById("offset").value);
   const textareaValue = document.getElementById("textarea").value;
-  const messageElement = document.getElementById("message");
+  const messageElement = document.getElementById("textarea2");
   const answer = cipher.encode(offsetValue, textareaValue);
 
   messageElement.value = answer;
@@ -16,6 +16,27 @@ decipherBtn.addEventListener("click", function () {
   const offsetValue = parseInt(document.getElementById("offset").value);
   const textareaValue = document.getElementById("textarea").value;
   const messageElement = document.getElementById("message");
+  const answer = cipher.decode(offsetValue, textareaValue);
+
+  messageElement.value = answer;
+})
+
+const cipherBtn2 = document.getElementById("cipher2");
+const decipherBtn2 = document.getElementById("decipher2");
+
+cipherBtn2.addEventListener("click", function () {
+  const offsetValue = parseInt(document.getElementById("offset").value);
+  const textareaValue = document.getElementById("textarea2").value;
+  const messageElement = document.getElementById("textarea");
+  const answer = cipher.encode(offsetValue, textareaValue);
+
+  messageElement.value = answer;
+})
+
+decipherBtn2.addEventListener("click", function(){
+  const offsetValue = parseInt(document.getElementById("offset").value);
+  const textareaValue = document.getElementById("textarea2").value;
+  const messageElement = document.getElementById("message2");
   const answer = cipher.decode(offsetValue, textareaValue);
 
   messageElement.value = answer;
